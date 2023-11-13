cb83b2cbb7e9abb2811503ffe6afd29303aaa899
SAMshashank <shashankkushwaha123@gmail.com>
2023-11-13 22:48:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Profile Screen Project
+
+This project is a frontend application built using React.js  that renders a profile screen similar to the provided screenshot from mobile view.
+
+![Profile Screen](https://www.terriblytinytales.com/profile.jpeg)
+
+## Project Overview
+
+The goal of this project is to create a consistent profile screen that renders correctly on different mobile devices. The profile data is hardcoded in a local JSON file, eliminating the need for a backend.
+
+## Technologies Used
+
+- React.js 
+- HTML
+- CSS
+
+## Getting Started
+
+Follow the instructions below to set up and run the project locally.
+
+![Profile Overview](https://reflectsmart.tech/images/Screenshot%202023-11-13%20222957.png?123) 
+
+## steps to creating this project
+  1. npx create-react-app 
+  2. Make Components
+  3. Add Json file
+  4. For styling I used css
+  5. I use FIGMA for creating UI
+  6. Adding className and HTML tags 
+  7. Adding css
+  8. Testing UI in different mobile  devices
+  9. Fixing few bugs
+  10. Make the Build ready ( NPM run build)
---
+++ src/pages/IPhone1415ProMax1.js
@@ -0,0 +1,108 @@
+import React from "react";
+import "./IPhone1415ProMax1.css";
+
+import jsonData from "./jsonData.json";
+const IPhone1415ProMax1 = () => {
+  return (
+    <div className="iphone-14-15-pro-max-1">
+      <div className="iphone-14-15-pro-max-1-child" />
+      <img className="ttt-1-icon" alt="" src={jsonData.tttIcon} />
+      <div className="div">{jsonData.divContent}</div>
+      <div className="stories">{jsonData.storiesText}</div>
+      <div className="iphone-14-15-pro-max-1-item" />
+      <div className="courses">{jsonData.coursesText}</div>
+      <img className="cover-pic-1-icon" alt="" src={jsonData.coverPic} />
+      <img className="user-1-1-icon" alt="" src={jsonData.userIcon} />
+      <div className="anuj-gosalia">{jsonData.userName}</div>
+      <img
+        className="iphone-14-15-pro-max-1-inner"
+        alt=""
+        src={jsonData.rectangleIcon}
+      />
+      <img className="rectangle-icon" alt="" src={jsonData.rectangleIcon} />
+      <div className="div1">{jsonData.div1Text}</div>
+      <div className="follower">{jsonData.followerText}</div>
+      <div className="follower1">{jsonData.followerText1}</div>
+      <div className="div2">{jsonData.div2Text}</div>
+      <div className="former-developer-at">{jsonData.formerDeveloperText}</div>
+      <div className="httpwwwinstagramcomanujg">{jsonData.instagramLink}</div>
+      <div className="div3">{jsonData.starIcon}</div>
+      <img className="like-1-icon" alt="" src={jsonData.likeIcon} />
+      <img className="like-1-icon" alt="" src={jsonData.likeIcon} />
+      <div className="div4">{jsonData.div4Text}</div>
+      <div className="div5">{jsonData.thumbIcon}</div>
+      <div className="div6">{jsonData.div6Text}</div>
+      <div className="k">{jsonData.kText}</div>
+      <div className="k1">{jsonData.k1Text}</div>
+      <img
+        className="iphone-14-15-pro-max-1-child1"
+        alt=""
+        src={jsonData.rectangle7Icon}
+      />
+      <div className="august-2-">{jsonData.august2Text}</div>
+      <div className="musing-by-shashank-container">
+        <span>{jsonData.musingText}</span>
+        <span className="by-shashank">{` by ${jsonData.author}`}</span>
+      </div>
+      <div className="the-world-is-container">
+        <p className="the-world-is">{jsonData.worldChangingText1}</p>
+        <p className="the-world-is">{jsonData.worldChangingText2}</p>
+        <p className="sadsadsadsasacascsadsadsadsads">
+          {jsonData.sadsadsadsText}
+        </p>
+      </div>
+      <div className="div7">{jsonData.thumbIcon}</div>
+      <div className="a-changing-world">{jsonData.articleTitle}</div>
+      <img
+        className="iphone-14-15-pro-max-1-child2"
+        alt=""
+        src={jsonData.rectangle7Icon}
+      />
+      <div className="august-2-1">{jsonData.august2Text1}</div>
+      <div className="musing-by-shashank-container1">
+        <span>{jsonData.musingText1}</span>
+        <span className="by-shashank">{` by ${jsonData.author}`}</span>
+      </div>
+      <div className="the-world-is-container1">
+        <p className="the-world-is">{jsonData.worldChangingText11}</p>
+        <p className="the-world-is">{jsonData.worldChangingText21}</p>
+        <p className="sadsadsadsasacascsadsadsadsads">
+          {jsonData.sadsadsadsText1}
+        </p>
+      </div>
+      <div className="div8">{jsonData.thumbIcon}</div>
+      <div className="a-changing-world1">{jsonData.articleTitle1}</div>
+      <img
+        className="iphone-14-15-pro-max-1-child3"
+        alt=""
+        src={jsonData.rectangle7Icon}
+      />
+      <div className="august-2-2">{jsonData.august2Text1}</div>
+      <div className="musing-by-shashank-container2">
+        <span>{jsonData.musingText1}</span>
+        <span className="by-shashank">{` by ${jsonData.author}`}</span>
+      </div>
+      <div className="the-world-is-container2">
+        <p className="the-world-is">{jsonData.worldChangingText2}</p>
+        <p className="the-world-is">{jsonData.worldChangingText22}</p>
+        <p className="sadsadsadsasacascsadsadsadsads">
+          {jsonData.sadsadsadsText2} 
+        </p>
+        <p className="sadsadsadsasacascsadsadsadsads">
+        {jsonData.sadsadsadsText22}
+        </p>
+      </div>
+      <div className="div9">{jsonData.thumbIcon}</div>
+      <div className="a-changing-world2">{jsonData.articleTitle2}</div>
+      <img className="vector-icon" alt="" src={jsonData.vectorIcon} />
+      <div className="posts">{jsonData.postsText}</div>
+      <div className="frame-div" />
+      <img className="diamond-1-icon" alt="" src={jsonData.diamondIcon} />
+      <img className="checked-1-icon" alt="" src={jsonData.checkedIcon} />
+      <img className="view-1-icon" alt="" src={jsonData.viewIcon} />
+      <img className="heart-1-icon" alt="" src={jsonData.heartIcon} />
+    </div>
+  );
+};
+
+export default IPhone1415ProMax1;
